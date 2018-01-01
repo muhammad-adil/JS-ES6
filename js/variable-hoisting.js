4207bc7c431f21d6c2b619da7f74dcb663bfd4e1
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-01-01 21:00:00 +0100

    let or hoisting work done

--- js/variable-hoisting.js
+++ js/variable-hoisting.js
@@ -1,8 +1,19 @@
 
+//x = 6;
 
+//elem = document.getElementById("divContainer"); //find an element
+//elem.innerHTML = x; //Display x in the element
+
+//var x; //Declare x
+//----------------------------------------
+
+
+
+//Hoisting is js default behaviour to declare all variable at top.
+//js only hoist declaration not initialization
+
+var x; //Declare x
 x = 6;
 
 elem = document.getElementById("divContainer"); //find an element
 elem.innerHTML = x; //Display x in the element
-
-var x; //Declare x
