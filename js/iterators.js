43657aa4caf909a612e9fbf626b6334a9c0882e9
unknown <adil.muhammad93@gmail.com>
2018-01-06 20:42:10 +0500

    iterators

---
+++ js/iterators.js
@@ -0,0 +1,14 @@
+"use strict";
+var words = ['skill', 'tech', 'idea'];
+
+//entries
+for(let word of words.entries())
+console.log(word)
+
+//values
+for(let word of words.values())
+console.log(word)
+
+//keys
+for(let word of words.keys())
+console.log(word)
