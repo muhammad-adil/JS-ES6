bc22d15b679a2f9797d4be01d032d790e9830668
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-01-08 17:22:30 +0500

    fetch

--- js/promise.js
+++ js/promise.js
@@ -1,5 +1,6 @@
 
 
+
 //jquery ajax way
 $.ajax("https://reqres.in/api/users",{
 	success: function(data){
@@ -12,4 +13,13 @@ $.ajax("https://reqres.in/api/users",{
 });
 
 
-//
+//fetch way
+fetch("https://reqres.in/api/users", {
+	method: 'get'
+}).then(function(response){
+	return response.json();
+}).then(function(data){
+	console.log(data.data);
+}).catch(function(){
+	console.log("failed to fetch data");
+});
