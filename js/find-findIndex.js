76a4fc6610b0d58b144fc5670abff0856f8995c7
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-11-22 16:35:46 +0100

    find findIndex function

--- js/find-findIndex.js
+++ js/find-findIndex.js
@@ -15,3 +15,15 @@ console.log("First number over 18 is " + firstFind);
 // The item value
 // The item index
 // The array itself
+
+// Array.findIndex()
+// The findIndex() method returns the index of the first array element that passes a test function.
+// This example finds the index of the first element that is larger than 20:
+
+// var numbersArr = [4, 9, 11, 14, 16, 20, 25, 29]; // as already define above
+var firstFindIndex = numbersArr.findIndex(firstFindIndexFunction);
+
+function firstFindIndexFunction(value, index, array) {
+  return value > 18;
+}
+console.log("First number over 20 has index " + firstFindIndex);
