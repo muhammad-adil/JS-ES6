ba8ce69dfd99ad3fe53f31f336da7161236f0b23
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-11-22 16:31:03 +0100

    find findIndex function

---
+++ js/defaultParameter.js
@@ -0,0 +1,13 @@
+
+
+
+// ES6 allows function parameters to have default values.
+
+ function addThenSum(x, y = 10) {
+    // y is 10 if not passed or undefined
+    return x + y;
+}
+addThenSum(5); // will return 15 
+
+
+
---
+++ js/find-findIndex.js
@@ -0,0 +1,17 @@
+// Array.find()
+// The find() method returns the value of the first array element that passes a test function.
+// This example finds (returns the value of ) the first element that is larger than 18:
+
+var numbersArr = [4, 9, 11, 14, 16, 20, 25, 29];
+var firstFind = numbersArr.find(findFunction);
+
+function findFunction(value, index, array) {
+  return value > 18;
+}
+
+console.log("First number over 18 is " + firstFind);
+
+//function takes 3 arguments:
+// The item value
+// The item index
+// The array itself
